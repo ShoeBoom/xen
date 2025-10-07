d1e40bf512ad860fa6a33ef10fcf923acf1aea07
Shubham Bhardwaj <15147944+ShoeBoom@users.noreply.github.com>
2025-10-07 02:17:25 -0400

    implement logic to handle new tab and externally opened tabs in background script

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,10 +1,26 @@
+const isNewTab = (url: string | undefined) => {
+  if (url === undefined) return true;
+
+  const regexes = [
+    /.*:\/\/newtab/,
+    /about:newtab/,
+    /about:home/,
+  ];
+  return regexes.some((regex) => regex.test(url));
+};
+
 export default defineBackground(() => {
   browser.tabs.onCreated.addListener(async (tab) => {
     if (tab.pinned) return;
     if (tab.id === undefined || tab.windowId === undefined) return;
-    const tabs = await browser.tabs.query({ windowId: tab.windowId });
-    const firstUnpinned = tabs.find((t) => !t.pinned);
-    const boundary = firstUnpinned ? firstUnpinned.index : tabs.length;
-    await browser.tabs.move(tab.id, { index: boundary });
+    const url = tab.pendingUrl;
+    // console.log({url, tab});
+    const isExternallyOpened = tab.openerTabId === undefined;
+    if (isNewTab(url) || isExternallyOpened) {
+      const tabs = await browser.tabs.query({ windowId: tab.windowId });
+      const firstUnpinned = tabs.find((t) => !t.pinned);
+      const boundary = firstUnpinned ? firstUnpinned.index : tabs.length;
+      await browser.tabs.move(tab.id, { index: boundary });
+    }
   });
 });
