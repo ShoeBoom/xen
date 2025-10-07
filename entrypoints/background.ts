2fd282128e6ac0f69e6d814826cf9b51835299ac
Shubham Bhardwaj <15147944+ShoeBoom@users.noreply.github.com>
2025-10-07 02:55:58 -0400

    refactor background script to use dynamic regex for new tab detection and improve comments for clarity

--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,11 +1,20 @@
+const buildRegexes = (paths: string[]) =>
+  paths.flatMap((p) => [
+    new RegExp(`^[a-zA-Z]+://${p}/?$`),
+    new RegExp(`^about:${p}/?$`),
+  ]);
+// some of these pages can be opended using "open in new tab"/target="_blank"
+// ideally we would detect this and not move the tab to the start.
+// but since openerTabId is set regardless of 
+//       if the it was just active while opening a new tab 
+//    vs the tab being responsible for opening the new tab
+// we will default to moving the tab to the start since 99% of the time these are not opened by another tab, 
+// should update this if my mental model is wrong.
+const regexes = buildRegexes(["newtab", "history", "downloads", "home"]);
+
 const isNewTab = (url: string | undefined) => {
   if (url === undefined) return true;
 
-  const regexes = [
-    /.*:\/\/newtab/,
-    /about:newtab/,
-    /about:home/,
-  ];
   return regexes.some((regex) => regex.test(url));
 };
 
@@ -14,7 +23,7 @@ export default defineBackground(() => {
     if (tab.pinned) return;
     if (tab.id === undefined || tab.windowId === undefined) return;
     const url = tab.pendingUrl;
-    // console.log({url, tab});
+    console.log({url, tab});
     const isExternallyOpened = tab.openerTabId === undefined;
     if (isNewTab(url) || isExternallyOpened) {
       const tabs = await browser.tabs.query({ windowId: tab.windowId });
